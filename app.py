5105c15c9a72e7b290dbf8b5e97295ee7f9cc53c
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-06-26 12:27:19 +0200

    importe etwas schöner

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from typing import OrderedDict
 from flask import Flask
 from flask.templating import render_template
 from flask.helpers import url_for
+from importlib import import_module
 
 app = Flask(__name__)
 
@@ -16,14 +17,17 @@ _plugins = [
 ]
 
 # register plugins
+modules = []
 for plugin in _plugins:
-    exec(f'from plugins.{plugin}.plugin import bp; app.register_blueprint(bp)')
+    p = import_module(f'plugins.{plugin}')
+    app.register_blueprint(p.bp)
+    modules.append(p)
 
 @app.route('/')
 def mainpage():
     tiles = OrderedDict()
-    for plugin in _plugins:
-        exec(f'from plugins.{plugin}.plugin import plugin_name, tile; tiles[plugin_name] = eval(tile)')
+    for p in modules:
+        tiles[p.plugin_name] = eval(p.tile)
     return render_template('scaffold.jinja2', tiles=tiles)
 
 # python -m pipenv run flask run
