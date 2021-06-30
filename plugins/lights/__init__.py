971257e6460f857d896c292daea4e6eca98d7836
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-06-30 17:11:48 +0200

    ifttt -> yeelight

--- plugins/lights/__init__.py
+++ plugins/lights/__init__.py
@@ -8,6 +8,7 @@ from random import choice
 import time
 import requests
 from . import sonoff
+from yeelight import Bulb
 
 plugin_name = 'licht'
 bp = Blueprint(plugin_name, __name__)
@@ -15,46 +16,60 @@ bp = Blueprint(plugin_name, __name__)
 tile = f"""dict(
             licht=dict(
                 __default=url_for('{plugin_name}.licht_more'),
-                _1=url_for(f'{plugin_name}.licht', bulb='wohnzimmer'),
-                _2=url_for(f'{plugin_name}.licht', bulb='esszimmer'),
-                _3=url_for(f'{plugin_name}.licht', bulb='flur'),
-                all=url_for('{plugin_name}.licht', bulb='all'),
+                _1=url_for(f'{plugin_name}.licht', bulb='wohnzimmer', mainpage=True),
+                _2=url_for(f'{plugin_name}.licht', bulb='esszimmer', mainpage=True),
+                _3=url_for(f'{plugin_name}.licht', bulb='flur', mainpage=True),
+                all=url_for('{plugin_name}.licht', bulb='all', mainpage=True),
             )
         )"""
 
 with open('plugins/lights/config.json') as config_file:
     config = json.load(config_file)
-    _key = config.get('yeelight').get('apikey')
+    _key = config.get('ifttt').get('apikey', '')
     _user = config.get('sonoff').get('user')
     _pass = config.get('sonoff').get('password')
     _region = config.get('sonoff').get('region')
-Y_TEMPLATE = "https://maker.ifttt.com/trigger/{trigger}/with/key/" + _key
+    _y_bulbs = config.get('yeelight').get('bulbs')
+IFTTT_TEMPLATE = "https://maker.ifttt.com/trigger/{trigger}/with/key/" + _key
 
 s_connection = sonoff.Sonoff(_user, _pass, _region)
 s_device_ids = {d['name']:d.get('deviceid') for d in s_connection.get_devices()}
-print(s_device_ids)
 
-def yeelight(trigger):
-    requests.get(Y_TEMPLATE.format(trigger=trigger))
+s_devices = {name:s_connection.get_device(dev_id) for name, dev_id in s_device_ids.items()}
+y_devices = {name:Bulb(ip) for name, ip in _y_bulbs.items()}
+
+
+def ifttt(trigger):
+    requests.get(IFTTT_TEMPLATE.format(trigger=trigger))
 
 
 def sonoff(device, state='toggle'):
     if state == 'toggle':
-        d = s_connection.get_device(s_device_ids[device])
+        d = s_devices[device]
         state = d['params']['switch']
         state = {'on':'off', 'off':'on'}[state]
     s_connection.switch(state, s_device_ids[device])
     time.sleep(.5)
 
 
+def yeelight(device, state='toggle'):
+    if state == 'toggle':
+        y_devices[device].toggle()
+    elif state == 'on':
+        y_devices[device].turn_on()
+    elif state == 'off':
+        y_devices[device].turn_off()
+    time.sleep(.5)
+
+
 @bp.route('/light/<bulb>')
 def licht(bulb:str=None):
     if bulb in ('wohnzimmer', 'all'):
-        yeelight('LightsWohnzimmer')
+        yeelight('wohnzimmer')
     if bulb in ('flur', 'all'):
-        yeelight('LightsFlur')
+        yeelight('flur')
     if bulb in ('schlafzimmer', 'all'):
-        yeelight('LightsSchlafzimmer')
+        yeelight('schlafzimmer')
     if bulb in ('esstisch', 'all'):
         sonoff('Esstisch')
     if bulb in ('ananas', 'all'):
@@ -67,6 +82,9 @@ def licht(bulb:str=None):
         sonoff('Esstisch')
         sonoff('Bild')
         sonoff('Ananas')
+    print(request.args)
+    if request.args.get('mainpage'):
+        return redirect(url_for('mainpage'))
     return redirect(url_for(f'{plugin_name}.licht_more'))
 
 
--- plugins/octoprint/__init__.py
+++ plugins/octoprint/__init__.py
@@ -3,6 +3,7 @@ from flask.helpers import url_for
 from flask.templating import render_template
 from werkzeug.utils import redirect
 from importlib import reload
+import time
 
 plugin_name = 'ender'
 bp = Blueprint(plugin_name, __name__)
@@ -35,6 +36,8 @@ def ender(cmd):
             octoprint.send_gcode('G1 Z100')
         elif cmd == 'home':
             octoprint.send_gcode('G28')
+        elif cmd == 'disable_steppers':
+            octoprint.send_gcode('M18 X Y')
         elif cmd == 'bed_heat':
             octoprint.heat_bed(65)
         elif cmd == 'bed_off':
@@ -43,6 +46,10 @@ def ender(cmd):
             octoprint.heat_extruder(215)
         elif cmd == 'tool_off':
             octoprint.heat_extruder(0)
+        elif cmd == 'status':
+            etl = octoprint.get_etl()
+            etl = time.strftime('%H:%M:%S', time.gmtime(etl))
+            flash(f'etl: {etl} ({octoprint.get_progress():.1f} %)')
         else:
             return cmd + ' not yet implemented'
     except Exception as e:
@@ -74,6 +81,8 @@ def ender_more():
                 heat=url_for(f'{plugin_name}.ender', cmd='tool_heat'),
                 off=url_for(f'{plugin_name}.ender', cmd='tool_off'),
             ),
-        )
+        ),
+        disable_steppers=url_for(f'{plugin_name}.ender', cmd='disable_steppers'),
+        print_status=url_for(f'{plugin_name}.ender', cmd='status'),
     )
     return render_template('scaffold.jinja2', tiles=tiles)
