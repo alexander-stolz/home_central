15393652f8076bf0f7ca90992466a7e0a69f8b68
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-06-30 11:57:24 +0200

    system: speedtest

--- plugins/lights/test.py
+++
@@ -1,3 +0,0 @@
-import speedtest
-
-speedtest
--- plugins/system/__init__.py
+++ plugins/system/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, flash
 from flask.helpers import url_for
 from flask.templating import render_template
 from werkzeug.utils import redirect
@@ -8,6 +8,7 @@ bp = Blueprint(plugin_name, __name__)
 
 tile = f"""dict(
             system=dict(
+                __default=url_for('{plugin_name}.system_more'),
                 shutdown=url_for('{plugin_name}.system', cmd='shutdown'),
                 cancel=url_for('{plugin_name}.system', cmd='cancel')
             ),
@@ -20,4 +21,17 @@ def system(cmd):
         os.system('shutdown -s -t 10')
     elif cmd == 'cancel':
         os.system('shutdown -a')
+    elif cmd == 'speedtest':
+        import speedtest
+        s = speedtest.Speedtest()
+        flash(f'down: {s.download() / 1e6:.2f} mbit')
+        flash(f'up: {s.upload() / 1e6:.2f} mbit')
     return redirect(url_for('mainpage'))
+
+
+@bp.route('/system')
+def system_more():
+    tiles = dict(
+        speedtest=url_for(f'{plugin_name}.system', cmd='speedtest')
+    )
+    return render_template('scaffold.jinja2', tiles=tiles)
