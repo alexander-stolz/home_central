dbc86af85b0a1e5bdbf6d0e1feb52c27d8e89072
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-06-30 11:35:36 +0200

    pipfile

--- plugins/lights/test.py
+++ plugins/lights/test.py
@@ -1,9 +1,3 @@
-import sonoff, time
+import speedtest
 
-s = sonoff.Sonoff('amstolz@gmail.com', R'Ewelink\3193*', 'eu')
-devices = s.get_devices()
-for d in devices:
-    name = d.get('name')
-    print(name)
-    s.switch('off', d.get('deviceid'))
-    time.sleep(1)
+speedtest
