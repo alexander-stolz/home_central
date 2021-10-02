37d1c03daf8eaedd2176ea4ffe58b6876111c782
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-10-02 12:33:17 +0200

    lockfile bei shutdown löschen

--- home_central.bat
+++ home_central.bat
@@ -1,3 +1,2 @@
 python update.py
 python -m pipenv run uvicorn app:asgi_app --host 0.0.0.0 --port 5000 --workers 1 --ws-ping-timeout 120 --timeout-keep-alive 60
-del lockfile
--- plugins/system/__init__.py
+++ plugins/system/__init__.py
@@ -1,3 +1,4 @@
+from os import remove
 from flask import Blueprint, flash, request, send_from_directory
 from flask.helpers import url_for
 from flask.templating import render_template
@@ -17,14 +18,18 @@ tile = f"""dict(
 
 UPLOADS = []
 
+
 @bp.route('/system/<cmd>', methods=['GET', 'POST'])
 def system(cmd):
     import os
     if cmd == 'shutdown':
+        remove('lockfile')
         os.system('shutdown -s -t 10')
     if cmd == 'restart':
+        remove('lockfile')
         os.system('shutdown -r -t 00')
     elif cmd == 'cancel':
+        open('lockfile', 'w').close()
         os.system('shutdown -a')
     elif cmd == 'speedtest':
         import speedtest
@@ -34,34 +39,33 @@ def system(cmd):
     elif cmd == 'upload':
         file = request.files['file']
         filename_orig = file.filename
-        filename_save = datetime.now().strftime('%Y%m%d%H%M%S') + '_' + filename_orig
+        filename_save = datetime.now().strftime(
+            '%Y%m%d%H%M%S') + '_' + filename_orig
         filename_save = secure_filename(filename_save)
         file.save(f'./static/{filename_save}')
         UPLOADS.append((filename_orig, filename_save))
     elif cmd == 'download':
-        return send_from_directory(
-            './static', UPLOADS[-1][1],
-            as_attachment=True,
-            attachment_filename=UPLOADS[-1][0]
-            )
+        return send_from_directory('./static',
+                                   UPLOADS[-1][1],
+                                   as_attachment=True,
+                                   attachment_filename=UPLOADS[-1][0])
 
     return redirect(url_for('mainpage'))
 
 
 @bp.route('/system')
 def system_more():
-    tiles = dict(
-        __10235=url_for('mainpage'),
-        speedtest=url_for(f'{plugin_name}.system', cmd='speedtest'),
-        restart=url_for(f'{plugin_name}.system', cmd='restart'),
-        share=dict(
-            share=dict(
-                send='#',
-                receive=url_for(f'{plugin_name}.system', cmd='download'),
-            )
-        )
-    )
-    return render_template('scaffold.jinja2', tiles=tiles, additional_footer=additional_footer)
+    tiles = dict(__10235=url_for('mainpage'),
+                 speedtest=url_for(f'{plugin_name}.system', cmd='speedtest'),
+                 restart=url_for(f'{plugin_name}.system', cmd='restart'),
+                 share=dict(share=dict(
+                     send='#',
+                     receive=url_for(f'{plugin_name}.system', cmd='download'),
+                 )))
+    return render_template('scaffold.jinja2',
+                           tiles=tiles,
+                           additional_footer=additional_footer)
+
 
 additional_footer = """
 <form id="upload-form" action="system/upload" method="post" enctype="multipart/form-data">
