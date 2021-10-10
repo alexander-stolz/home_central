a160302680d7a3b2f5ddfa359ed4a50800d61687
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-10-10 10:34:11 +0200

    logging

--- home_central.bat
+++ home_central.bat
@@ -1,2 +1,2 @@
 python update.py
-python -m pipenv run uvicorn app:asgi_app --host 0.0.0.0 --port 5000 --workers 1 --ws-ping-timeout 120 --timeout-keep-alive 60
+python -m pipenv run uvicorn app:asgi_app --host 0.0.0.0 --port 5000 --workers 1 --ws-ping-timeout 120 --timeout-keep-alive 60 --log-level 'warning'
--- home_central.sh
+++ home_central.sh
@@ -1,2 +1,2 @@
 python3 update.py
-python3 -m pipenv run uvicorn app:asgi_app --host 0.0.0.0 --port 5000 --workers 1 --ws-ping-timeout 120 --timeout-keep-alive 60
+python3 -m pipenv run uvicorn app:asgi_app --host 0.0.0.0 --port 5000 --workers 1 --ws-ping-timeout 120 --timeout-keep-alive 60 --log-level 'warning'
