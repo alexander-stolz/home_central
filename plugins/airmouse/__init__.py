b3abdfcf88e4b290fb03d4105599cc42977d5259
Alexander M. Stolz <alexander.stolz@dvs.ag>
2021-10-12 19:43:08 +0200

    keys

--- plugins/airmouse/__init__.py
+++ plugins/airmouse/__init__.py
@@ -57,19 +57,12 @@ def airmouse():
                 keyboard.press(Key.media_volume_down)
                 keyboard.release(Key.media_volume_down)
         elif res.get('type') == 'key':
-            _key = chr(res['key'])
-            _shift = res['shift']
+            _key = res['key']
             _ctrl = res['ctrl']
-            if 'A' <= _key <= 'Z':
-                _key = _key.lower()
-            if _shift:
-                keyboard.press(Key.shift)
             if _ctrl:
                 keyboard.press(Key.ctrl)
             keyboard.press(_key)
             keyboard.release(_key)
-            if _shift:
-                keyboard.release(Key.shift)
             if _ctrl:
                 keyboard.release(Key.ctrl)
         elif res.get('type') == 'mouse':
