ce4f49cadfd5d29456c5e10083f5fe4777863dfb
Alexander M. Stolz <alexander.stolz@dvs.ag>
2022-02-06 12:34:30 +0100

    ambicam controls

--- plugins/ambicam/__init__.py
+++ plugins/ambicam/__init__.py
@@ -1,13 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 import json
+import urllib
 
 plugin_name = 'ambicam'
 bp = Blueprint(plugin_name, __name__)
 
 tile = f"""dict(
-            ambilight=dict(
+            ambicam=dict(
                 __default='#',
                 start=url_for('{plugin_name}.start'),
                 stop=url_for('{plugin_name}.stop'),
@@ -20,15 +21,50 @@ with open(f'plugins/{plugin_name}/config.json') as config_file:
 
 @bp.route('/ambicam/start')
 def start():
-    import urllib
-
-    urllib.request.urlopen(f'http://{config["host"]}:{config["port"]}/start')
+    urllib.request.urlopen(f'http://{config["host"]}:{config["port"]}/start/force')
     return redirect(url_for('mainpage'))
 
 
 @bp.route('/ambicam/stop')
 def stop():
-    import urllib
-
     urllib.request.urlopen(f'http://{config["host"]}:{config["port"]}/stop')
     return redirect(url_for('mainpage'))
+
+
+@bp.route('/ambicam_color/<cmd>')
+def ambicam_color(cmd):
+    urllib.request.urlopen(f'http://{config["host"]}:{config["port"]}/color/{cmd}')
+    return redirect(url_for(f'{plugin_name}.ambicam_more'))
+
+
+@bp.route('/ambicam')
+def ambicam_more():
+    tiles = dict(
+        __10235=url_for('mainpage'),
+        auto_colors=url_for(f'{plugin_name}.ambicam_color', cmd='auto_colors'),
+        red=dict(
+            red=dict(
+                __8722=url_for(f'{plugin_name}.ambicam_color', cmd='red_down'),
+                __43=url_for(f'{plugin_name}.ambicam_color', cmd='red_up'),
+            ),
+        ),
+        green=dict(
+            green=dict(
+                __8722=url_for(f'{plugin_name}.ambicam_color', cmd='green_down'),
+                __43=url_for(f'{plugin_name}.ambicam_color', cmd='green_up'),
+            ),
+        ),
+        blue=dict(
+            blue=dict(
+                __8722=url_for(f'{plugin_name}.ambicam_color', cmd='blue_down'),
+                __43=url_for(f'{plugin_name}.ambicam_color', cmd='blue_up'),
+            ),
+        ),
+        brightness=dict(
+            brightness=dict(
+                __8722=url_for(f'{plugin_name}.ambicam_color', cmd='brightness_down'),
+                __43=url_for(f'{plugin_name}.ambicam_color', cmd='brightness_up'),
+            ),
+        ),
+    )
+    return render_template('scaffold.jinja2', tiles=tiles)
